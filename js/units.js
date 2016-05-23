94a25b0d9f58db19a188306081d7bc0a3bc0bfba
benorg <benorg@me.com>
2016-05-23 15:03:05 -0400

    data table

---
+++ js/units.js
@@ -0,0 +1,56 @@
+module.exports = {
+  const units = {
+    length: {
+      ct: 'm',
+      m: 1,
+      mm: 0.001,
+      cm: 0.01,
+      dm: 0.1,
+      dam: 10,
+      hm: 100,
+      km: 1000,
+      in: 0.0254,
+      ft: 0.3048,
+      yd: 0.9144,
+      mi: 1609.34,
+      AU: 1.496e11,
+      ly: 9.461e15,
+      pc: 3.086e16,
+      ℓP: {
+        val: 1.6e35,
+        html: 'ℓ<sub>P</sub>'
+      },
+
+    area: {
+
+    },
+
+    volume: {
+
+    },
+
+    time: {
+
+    },
+
+    temp: {
+
+    },
+
+    mass: {
+
+    },
+
+    force: {
+
+    },
+
+    pressure: {
+
+    },
+
+    energy: {
+
+    }
+  };
+}
