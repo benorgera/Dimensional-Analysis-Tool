0f408db9a8c650d60130db12ba8b0c0ccd579e3e
benorg <benorg@me.com>
2016-05-24 14:31:18 -0400

    all units added

--- js/units.js
+++ js/units.js
@@ -1,162 +1,151 @@
 module.exports = {
   const units = {
-    length: {
-      ct: 'm',
-      m: 1,
-      mm: 0.001,
-      cm: 0.01,
-      dm: 0.1,
-      dam: 10,
-      hm: 100,
-      km: 1000,
-      in: 0.0254,
-      ft: 0.3048,
-      yd: 0.9144,
-      mi: 1609.34,
-      AU: 1.496e11,
-      ly: 9.461e15,
-      pc: 3.086e16,
-      ℓP: {
-        val: 1.6e-35,
-        html: 'ℓ<sub>P</sub>'
-      },
-
-    area: {
-      ct: 'sq m',
-      sq m: {
-        val:1,
-        html:'m<sup>2</sup>'
-      }
-      sq mm: {
-        val:0.000001,
-        html:'mm<sup>2</sup>'
-      }
-      sq cm: {
-        val:0.0001,
-        html:'cm<sup>2</sup>'
-      }
-      sq dm: {
-        val:0.01,
-        html:'dm<sup>2</sup>'
-      }
-      sq dam: {
-        val:100,
-        html:'dam<sup>2</sup>'
-      }
-      sq hm: {
-        val:10000,
-        html:'hm<sup>2</sup>'
-      }
-      sq km: {
-        val:1000000,
-        html:'km<sup>2</sup>'
-      }
-      sq in: {
-        val:0.00064516,
-        html:'in<sup>2</sup>'
-      }
-      sq ft: {
-        val:0.09290304,
-        html:'ft<sup>2</sup>'
-      sq yd: {
-        val:0.83612736,
-        html:'yd<sup>2</sup>'
-      sq mi: {
-        val:2589988.110336,
-        html:'mi<sup>2</sup>'
-      ac: 4046.8564224,
-      ha: 10000,
+    "Length": {
+      "ct": "m",
+      "m": 1,
+      "mm": 0.001,
+      "cm": 0.01,
+      "dm": 0.1,
+      "dam": 10,
+      "hm": 100,
+      "km": 1000,
+      "in": 0.0254,
+      "ft": 0.3048,
+      "yd": 0.9144,
+      "mi": 1609.34,
+      "AU": 1.496e11,
+      "ly": 9.461e15,
+      "pc": 3.086e16,
+      "ℓ<sub>P</sub>": 1.6e-35
     },
 
-    volume: {
-      ct:'L',
-      mL: 0.001,
-      cL: 0.01,
-      dL: 0.1,
-      daL: 10,
-      hL: 100,
-      kL: 1000,
-      cub m: {
-        val:1000,
-        html:'m<sup>3</sup>'
-      }
-      cub mm: {
-        val:1e-6,
-        html:'mm<sup>3</sup>'
-      cub cm: {
-        val:0.001,
-        html:'cm<sup>3</sup>'
-      cub dm: {
-        val:1,
-        html:'dm<sup>3</sup>'
-      cub dam: {
-        val:1000000,
-        html:'dam<sup>3</sup>'
-      cub hm: {
-        val:1000000000,
-        html:'hm<sup>3</sup>'
-      }
-      cub km: {
-        val:1000000000000,
-        html:'km<sup>3</sup>'
-      }
-      cub in:{
-        val:0.016387064,
-        html:'in<sup>3</sup>'
-      }
-      cub ft: {
-        val:28.316846592,
-        html: 'ft<sup>3</sup>'
-      }
-      pint: {
-        val:0.473176473,
-        type:'US'
-      }
-      pint: {
-        val:0.56826125,
-        type:'Imperial'
-      }
-      gallon: {
-        val:3.785411784,
-        type:'US'
-      }
-      gallon: {
-        val:4.54609,
-        type:'Imperial'
-      }
-      quart:{
-        val:0.946353,
-        type:'US'
-      }
-      quart:{
-        val:1.13652,
-        type:'Imperial'
-      }
-
+    "Area": {
+      "ct": "m<sup>2</sup>",
+      "m<sup>2</sup>": 1,
+      "mm<sup>2</sup>": 0.000001,
+      "cm<sup>2</sup>": 0.0001,
+      "dm<sup>2</sup>": 0.01,
+      "dam<sup>2</sup>": 100,
+      "hm<sup>2</sup>": 10000,
+      "km<sup>2</sup>": 1000000,
+      "in<sup>2</sup>": 0.00064516,
+      "ft<sup>2</sup>": 0.09290304,
+      "yd<sup>2</sup>": 0.83612736,
+      "mi<sup>2</sup>": 2589988.110336,
+      "ac": 4046.8564224,
+      "ha": 10000
+    },
+    
+    "Volume": {
+      "ct": "L",
+      "L": 1,
+      "mL": 0.001,
+      "cL": 0.01,
+      "dL": 0.1,
+      "daL": 10,
+      "hL": 100,
+      "kL": 1000,
+      "m<sup>3</sup>": 1000,
+      "mm<sup>3</sup>": 1e-6,
+      "cm<sup>3</sup>": 0.001,
+      "dm<sup>3</sup>": 1,
+      "dam<sup>3</sup>": 1000000,
+      "hm<sup>3</sup>": 1000000000,
+      "km<sup>3</sup>": 1000000000000,
+      "in<sup>3</sup>": 0.016387064,
+      "ft<sup>3</sup>": 28.316846592,
+      "gal<sub>US</sub>": 3.785411784,
+      "pt<sub>US</sub>": 0.473176473,
+      "qt<sub>US</sub>": 0.946353,
+      "cup<sub>US</sub>": 0.236588,
+      "fl oz<sub>US</sub>": 0.0295735,
+      "gal<sub>SI</sub>": 4.54609,
+      "pt<sub>SI</sub>": 0.56826125,
+      "qt<sub>SI</sub>": 1.13652,
+      "cup<sub>SI</sub>": 0.284131,
+      "fl oz<sub>SI</sub>": 0.0284131
     },
 
-    time: {
-
+    "Time": {
+      "ct": "s",
+      "s": 1,
+      "min": 60,
+      "hr": 3600,
+      "day": 86400,
+      "week": 604800,
+      "month": 2.6298e6,
+      "year": 31557600,
+      "decade": 315576000,
+      "century": 3155760000,
+      "millenium": 31557600000,
+      "fortnight": 1209600,
+      "t<sub>P</sub>": 5.39056e-44
     },
 
-    temp: {
-
+    "Temperature": {
+      "ct": NaN,
+      "K": NaN,
+      "R": NaN,
+      "°C": NaN,
+      "°F": NaN
     },
 
-    mass: {
-
+    "Mass": {
+      "ct": "g",
+      "g": 1,
+      "mg": 0.001,
+      "cg": 0.01,
+      "dg": 0.1,
+      "dag": 10,
+      "hg": 100,
+      "kg": 1000,
+      "ton<sub>US</sub>": 907184.74,
+      "ton<sub>SI</sub>": 1000000,
+      "oz": 28.35,
+      "sl": 14593.903,
+      "lb": 453.592,
+      "amu": 1.661e-24,
+      "m<sub>P</sub>": 2.17645e-5
     },
 
-    force: {
-
+    "Force": {
+      "ct": "N",
+      "N": 1,
+      "lb": 4.44822162825,
+      "F<sub>P</sub>": 1.21027e44
     },
 
-    pressure: {
-
+    "Pressure": {
+      "ct": "Pa",
+      "Pa": 1,
+      "bar": 100000,
+      "atm": 101325,
+      "psi": 6894.757,
+      "torr": 133.322
     },
 
-    energy: {
+    "Energy": {
+      "ct": "J",
+      "J": 1,
+      "kJ": 1000,
+      "ft·lb<sub>f</sub>": 1.356, 
+      "btu": 1055.056,
+      "thm": 105480400,
+      "cal": 4.184,
+      "kcal": 4184,
+      "kWh": 3.6e6,
+      "Wh": 3600,
+      "eV": 1.6022e-19,
+      "MeV": 1.6022e-13
+    },
 
-    }
+    // "Digital Storage": {
+    //   "ct": "byte",
+    //   "byte": 1,
+    //   "bit": 0.125,
+    //   "KB": 1024,
+    //   "MB": 
+    //   "GB": 
+    // }
   };
 }
