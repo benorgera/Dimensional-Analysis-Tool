a010f66335fde50065e3e9eb9480117f7c478ff3
jkalodner <jacob.kalodner@gmail.com>
2016-05-24 11:05:16 -0400

    added units

--- js/units.js
+++ js/units.js
@@ -17,15 +17,121 @@ module.exports = {
       ly: 9.461e15,
       pc: 3.086e16,
       ℓP: {
-        val: 1.6e35,
+        val: 1.6e-35,
         html: 'ℓ<sub>P</sub>'
       },
 
     area: {
-
+      ct: 'sq m',
+      sq m: {
+        val:1,
+        html:'m<sup>2</sup>'
+      }
+      sq mm: {
+        val:0.000001,
+        html:'mm<sup>2</sup>'
+      }
+      sq cm: {
+        val:0.0001,
+        html:'cm<sup>2</sup>'
+      }
+      sq dm: {
+        val:0.01,
+        html:'dm<sup>2</sup>'
+      }
+      sq dam: {
+        val:100,
+        html:'dam<sup>2</sup>'
+      }
+      sq hm: {
+        val:10000,
+        html:'hm<sup>2</sup>'
+      }
+      sq km: {
+        val:1000000,
+        html:'km<sup>2</sup>'
+      }
+      sq in: {
+        val:0.00064516,
+        html:'in<sup>2</sup>'
+      }
+      sq ft: {
+        val:0.09290304,
+        html:'ft<sup>2</sup>'
+      sq yd: {
+        val:0.83612736,
+        html:'yd<sup>2</sup>'
+      sq mi: {
+        val:2589988.110336,
+        html:'mi<sup>2</sup>'
+      ac: 4046.8564224,
+      ha: 10000,
     },
 
     volume: {
+      ct:'L',
+      mL: 0.001,
+      cL: 0.01,
+      dL: 0.1,
+      daL: 10,
+      hL: 100,
+      kL: 1000,
+      cub m: {
+        val:1000,
+        html:'m<sup>3</sup>'
+      }
+      cub mm: {
+        val:1e-6,
+        html:'mm<sup>3</sup>'
+      cub cm: {
+        val:0.001,
+        html:'cm<sup>3</sup>'
+      cub dm: {
+        val:1,
+        html:'dm<sup>3</sup>'
+      cub dam: {
+        val:1000000,
+        html:'dam<sup>3</sup>'
+      cub hm: {
+        val:1000000000,
+        html:'hm<sup>3</sup>'
+      }
+      cub km: {
+        val:1000000000000,
+        html:'km<sup>3</sup>'
+      }
+      cub in:{
+        val:0.016387064,
+        html:'in<sup>3</sup>'
+      }
+      cub ft: {
+        val:28.316846592,
+        html: 'ft<sup>3</sup>'
+      }
+      pint: {
+        val:0.473176473,
+        type:'US'
+      }
+      pint: {
+        val:0.56826125,
+        type:'Imperial'
+      }
+      gallon: {
+        val:3.785411784,
+        type:'US'
+      }
+      gallon: {
+        val:4.54609,
+        type:'Imperial'
+      }
+      quart:{
+        val:0.946353,
+        type:'US'
+      }
+      quart:{
+        val:1.13652,
+        type:'Imperial'
+      }
 
     },
 
